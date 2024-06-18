dc5a2b3490b9def0485be574bb8ddabaebd986a6
99omniaashraf <omniaashraf249@gmail.com>
2024-06-18 19:33:47 +0300

    Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+
+def log_stats():
+    # Connect to MongoDB
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    # Total number of logs
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods statistics
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    # Number of documents with method=GET and path=/status
+    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    log_stats()
