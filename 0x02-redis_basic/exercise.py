3d83c1c9ebd12f22f842ab409e9312f42f848b81
99omniaashraf <omniaashraf249@gmail.com>
2024-06-20 01:31:51 +0300

    redis_basic

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,7 @@ import uuid
 from typing import Union, Callable, Optional
 from functools import wraps
 
+
 def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
@@ -17,37 +18,41 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         input_key = f"{method.__qualname__}:inputs"
         output_key = f"{method.__qualname__}:outputs"
-        
+
         # Store the input arguments in Redis
         self._redis.rpush(input_key, str(args))
-        
+
         # Call the original method and get the output
         output = method(self, *args, **kwargs)
-        
+
         # Store the output in Redis
         self._redis.rpush(output_key, str(output))
-        
+
         return output
     return wrapper
+
 
 def replay(method: Callable):
     self = method.__self__
     input_key = f"{method.__qualname__}:inputs"
     output_key = f"{method.__qualname__}:outputs"
-    
+
     inputs = self._redis.lrange(input_key, 0, -1)
     outputs = self._redis.lrange(output_key, 0, -1)
-    
+
     call_count = len(inputs)
     print(f"{method.__qualname__} was called {call_count} times:")
-    
+
     for input_data, output_data in zip(inputs, outputs):
-        print(f"{method.__qualname__}(*{input_data.decode('utf-8')}) -> {output_data.decode('utf-8')}")
+        print(f"{method.__qualname__}(*
+              {input_data.decode('utf-8')}) -> {output_data.decode('utf-8')}")
+
 
 class Cache:
     def __init__(self):
@@ -61,7 +66,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn:
+            Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
         value = self._redis.get(key)
         if value is None:
             return None
